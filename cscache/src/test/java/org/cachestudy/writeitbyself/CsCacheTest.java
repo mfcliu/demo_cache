bfc65a25e5c9ad531457556800b80d91c46fed30
liujingyu <mfcliu@163.com>
2017-01-27 14:13:32 +0800

    fix helloworld

--- cscache/src/test/java/org/cachestudy/writeitbyself/CsCacheTest.java
+++ cscache/src/test/java/org/cachestudy/writeitbyself/CsCacheTest.java
@@ -1,40 +1,14 @@
 package org.cachestudy.writeitbyself;
 
 import org.cachestudy.writeitbyself.store.impl.BasicDataStore;
+import org.junit.Test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
-public class CsCacheTest extends TestCase {
-	/**
-	 * Create the test case
-	 *
-	 * @param testName name of the test case
-	 */
-	public CsCacheTest(String testName) {
-		super(testName);
+public class CsCacheTest {
+	@Test
+	public void TestHelloWorld() {
 		CsCache<String, String> cache = new CsCache<String, String>(new BasicDataStore<String, String>());
 		String key = "Hello";
 		cache.put(key, "World!");
 		System.out.println(key + " " + cache.get(key));
-		assertTrue(true);
-	}
-
-	/**
-	 * @return the suite of tests being tested
-	 */
-	public static Test suite() {
-		return new TestSuite(CsCacheTest.class);
-	}
-
-	/**
-	 * Rigourous Test :-)
-	 */
-	public void testApp() {
-		assertTrue(true);
 	}
 }
